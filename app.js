2ea34a35d37effd3548c7560644dbe3ac02e2325
minhhoccode111 <minhhoccode111@gmail.com>
2023-09-15 20:25:29 +0700

    The Modules Pattern

--- app.js
+++ app.js
@@ -1,172 +1,94 @@
-// console.log("Hello, world!");
-// ("use strict");
-// const EMPTY_NUM = 0;
-// const EMPTY_STR = "";
+console.log('Hello, World!');
 
-// let operator = "";
-// let currentNum = "";
-// let previousNum = "";
-// window.addEventListener("keypress", (e) => {
-//   keypressHandler(e.key);
-// });
+window.addEventListener('DOMContentLoaded', (e) => {
+  'use strict';
 
-// const numButtons = document.querySelectorAll("[data-number]");
-// numButtons.forEach((button) => {
-//   button.addEventListener("click", (e) => {
-//     handleNumberClick(e.target.innerText);
-//   });
-// });
+  const CreateNumber = () => {
+    let _value = '0';
+    const reset = () => (_value = '0');
+    // add a string number
+    const push = (v) => {
+      if (_value === '0') return (_value = v);
+      _value += v;
+    };
 
-// const operatorButtons = document.querySelectorAll("[data-operator]");
-// operatorButtons.forEach((button) => {
-//   button.addEventListener("click", (e) => {
-//     handleOperatorClick(e.target.innerText);
-//   });
-// });
+    // remove a last string number
+    const pop = () => {
+      if (_value === '0') return;
+      if (_value.length === 1) {
+        _value = '0';
+        return;
+      }
+      _value = _value.slice(0, _value.length - 1);
+    };
 
-// const formattedNumber = document.querySelectorAll("[data-formatted-number]");
-// const deleteButton = document.querySelector("[data-delete]");
-// const clearButton = document.querySelector("[data-all-clear]");
-// const operatorDisplay = document.querySelector(".operation-screen");
-// const numDisplay = document.querySelector(".number-screen");
-// numDisplay.innerText = "0";
-// const dotButton = document.querySelector("#btn-dot");
+    const get = () => _value;
 
-// clearButton.onclick = () => clearAll();
-// const clearAll = () => {
-//   operator = "";
-//   currentNum = "";
-//   previousNum = "";
-//   numDisplay.innerText = "0";
-//   operatorDisplay.innerText = "";
-// };
-// deleteButton.onclick = () => clearOne();
-// const clearOne = () => {
-//   currentNum = currentNum.toString().slice(0, -1);
-//   numDisplay.innerText = currentNum;
-//   if (currentNum === "") {
-//     numDisplay.innerText = "0";
-//   }
-// };
-// const handleNumberClick = (number) => {
-//   if (number === "." && currentNum === "") {
-//     currentNum = "0";
-//   }
-//   if (
-//     (number === "0" && currentNum === "") ||
-//     (number === "." && currentNum.includes("."))
-//   ) {
-//     return;
-//   }
+    return {
+      reset,
+      push,
+      pop,
+      get,
+    };
+  };
 
-//   if (previousNum !== "" && operator !== "") {
-//     operatorDisplay.innerText = previousNum + " " + operator;
-//   }
-//   if (currentNum.length < 9) {
-//     currentNum += number;
-//     numDisplay.innerText = currentNum;
-//   }
-// };
-// const handleOperatorClick = (op) => {
-//   if (currentNum === "") {
-//     return;
-//   }
-//   if (currentNum !== "" && previousNum === "" && operator === "") {
-//     previousNum = currentNum;
-//     currentNum = "";
-//     operator = op;
-//     operatorDisplay.innerText = previousNum + " " + operator;
-//   }
-//   if (currentNum !== "" && previousNum !== "" && operator !== "") {
-//     //calculate while currentNum,previousNum,operator are not equal ''
-//     //then assign result to previousNum and reset currentNum=''
-//     //assign new op to old operator
-//     //display previousNum to operatorDisplay with new operator
-//     calculate();
-//     operator = op;
-//     numDisplay.innerText = previousNum;
-//     operatorDisplay.innerText = "";
-//   }
-// };
-// const displayResult = () => {
-//   if (previousNum.toString().length >= 4) {
-//     let a = previousNum.toString().slice(0, 6) + "...";
-//     numDisplay.innerText = a;
-//     operatorDisplay.innerText = "";
-//     return;
-//   } else if (previousNum.toString().length < 3) {
-//     numDisplay.innerText = previousNum;
-//     operatorDisplay.innerText = "";
-//     return;
-//   }
-// };
-// const calculate = () => {
-//   currentNum == "" ? (currentNum = 0) : (currentNum = Number(currentNum));
-//   previousNum = Number(previousNum);
-//   switch (operator) {
-//     case "+":
-//       previousNum += currentNum;
-//       currentNum = "";
-//       displayResult();
-//       break;
-//     case "-":
-//       previousNum -= currentNum;
-//       currentNum = "";
-//       displayResult();
-//       break;
-//     case "×":
-//       previousNum *= currentNum;
-//       currentNum = "";
-//       displayResult();
-//       break;
-//     case "÷":
-//       previousNum /= currentNum;
-//       currentNum = "";
-//       displayResult();
-//       console.log(previousNum);
-//       break;
-//   }
-// };
+  const current = (() => {
+    const _num0 = CreateNumber();
+    const _num1 = CreateNumber();
+    let inputNum = _num0;
+    let _operator = '';
 
-// const equalButton = document.querySelector("[data-equals]");
-// equalButton.onclick = () => {
-//   if (currentNum != "" && previousNum != "" && operator != "") {
-//     calculate();
-//     currentNum = previousNum;
-//     numDisplay.innerText = currentNum.toString().slice(0, 9);
-//     previousNum = "";
-//     operator = "";
-//   }
-//   if (currentNum != "" && previousNum == "" && operator == "") {
-//     numDisplay.innerText = currentNum;
-//   }
-// };
+    const getCurrentInputNum = () => inputNum.get();
 
-// const keypressHandler = (e) => {
-//   // e.preventDefault();
-//   if ((e >= 0 && e <= 9) || e == ".") {
-//     handleNumberClick(e);
-//   }
-//   if (e == "Enter" || (e == "=" && currentNum !== "" && previousNum !== "")) {
-//     calculate();
-//     currentNum = previousNum;
-//     numDisplay.innerText = currentNum.toString().slice(0, 9);
-//     previousNum = "";
-//     operator = "";
-//   }
-//   if (e == "+" || e == "-") {
-//     handleOperatorClick(e);
-//   }
-//   if (e == "*") {
-//     handleOperatorClick("×");
-//   }
-//   if (e == "/") {
-//     handleOperatorClick("÷");
-//   }
-//   if (e == "Backspace") {
-//     clearOne();
-//   }
-//   if (e == "Escape") {
-//     clearAll();
-//   }
-// };
+    const switchNum = () => (inputNum === _num0 ? (inputNum = _num1) : (inputNum = _num0));
+
+    const getOperator = () => _operator;
+    const setOperator = (v) => (_operator = v);
+
+    const clear = () => {
+      inputNum = _num0;
+      _num0.reset();
+      _num1.reset();
+      _operator = '';
+    };
+
+    return {
+      clear,
+      switchNum,
+      getOperator,
+      setOperator,
+    };
+  })();
+
+  const display = (() => {
+    const operationScr = document.getElementById('operation-screen');
+    const numberScr = document.getElementById('number-screen');
+
+    const clear = () => {};
+
+    return {
+      clear,
+    };
+  })();
+
+  const app = (() => {
+    const clear = () => {
+      display.clear();
+      current.clear();
+    };
+    return {
+      clear,
+    };
+  })();
+
+  // define all buttons
+  const ac = document.getElementById('btn-ac');
+  const ce = document.getElementById('btn-ce');
+  const percent = document.getElementById('btn-percent');
+  const negation = document.getElementById('btn-negation');
+  const equal = document.getElementById('btn-equal');
+  const numbers = document.querySelectorAll('[data-number]');
+  const operations = document.querySelectorAll('[data-operator]');
+
+  // all buttons' even listeners
+});
