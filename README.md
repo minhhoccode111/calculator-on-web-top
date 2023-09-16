c21295094543d724a8e07cdbed08bcb7119ab1bb
minhhoccode111 <minhhoccode111@gmail.com>
2023-09-16 22:11:14 +0700

    some changes

--- README.md
+++ README.md
@@ -12,7 +12,41 @@ what I've learned :
 - It need more test and fix
 - and do two features 'percent' and 'minus'
 - and a lot more...
+- `add()` method are complicated with so many conditions and possibilities to check
 
-[View all projects' live demos](https://minhhoccode111.github.io/all-projects-live-demos/) 
+```js
+  add(numStr) {
+    if (numStr === '/' || numStr === '+' || numStr === '*') return; // ignore
+    if (numStr === '0' && this.current === '-') return; // ignore
 
-[Calculator on Web TOP](https://minhhoccode111.github.io/calculator-on-web-top/) 
+    if (numStr === '-') {
+      if (this.current === '0') this.current = '-'; // make it negative
+      if (this.current === '-') this.current = '0'; // make it positive
+      if (this.current !== '0') return; // ignore if at the middle of current
+      Display.mainScreen(this.current);
+      return;
+    }
+    if (numStr === '.') {
+      if (this.current.includes('.')) return; // ignore if more than 1 dot
+      if (this.current === '-') {
+        this.current = '-0.'; // change to this if already negative
+      } else {
+        this.current += numStr; // else just add to the end
+      }
+      Display.mainScreen(this.current);
+      return;
+    }
+    if (this.current === '0') {
+      if (numStr === '0') return; // ignore
+      this.current = numStr; // change if at the beginning
+      Display.mainScreen(this.current);
+      return;
+    }
+    this.current += numStr; // add a string at the end
+    Display.mainScreen(this.current);
+  }
+```
+
+[View all projects' live demos](https://minhhoccode111.github.io/all-projects-live-demos/)
+
+[Calculator on Web TOP](https://minhhoccode111.github.io/calculator-on-web-top/)
--- app.js
+++ app.js
@@ -8,71 +8,94 @@ class Display {
 
   static main = document.getElementById('number-screen');
 
+  static error = document.getElementById('error-message');
+
   static mainScreen(num) {
-    Display.main.textContent = num;
+    this.main.textContent = num;
   }
 
-  static supportScreen(num, operator) {
-    Display.support.textContent = num === 0 ? '' : num + operator;
+  static supportScreen(numStr, operator) {
+    // don't display if num===0
+    this.support.textContent = numStr + (operator || '');
+  }
+
+  static errorMessage() {
+    const str = `Please don't try to break my calculator :<`;
+    this.error.textContent = str;
+    setTimeout(() => {
+      this.error.textContent = '';
+    }, 2000);
   }
 }
 
 // used to create calculator with all functionalities
 class Calculator {
   constructor() {
-    this.previous = 0;
-    this.current = 0;
-    this.operator = '+';
-    this.result = 0;
+    this.previous = '0';
+    this.current = '0';
+    this.result = '0';
   }
 
-  update() {
-    this.previous = this.result;
-    this.current = 0;
+  add(numStr) {
+    if (numStr === '0' && this.current === '-') return Display.errorMessage(); // ignore
 
-    Display.mainScreen(this.current);
-    Display.supportScreen(this.previous, this.operator);
-  }
-
-  add(num) {
-    this.current = Number(this.current + num); // add a string at the end and convert back
+    if (numStr === '.') {
+      if (this.current.includes('.')) return Display.errorMessage(); // ignore if more than 1 dot
+      if (this.current === '-') {
+        this.current = '-0.'; // change to this if already negative
+      } else {
+        this.current += numStr; // else just add to the end
+      }
+      Display.mainScreen(this.current);
+      return;
+    }
+    if (this.current === '0') {
+      if (numStr === '0') return Display.errorMessage(); // ignore
+      this.current = numStr; // change if at the beginning
+      Display.mainScreen(this.current);
+      return;
+    }
+    this.current += numStr; // add a string at the end
     Display.mainScreen(this.current);
   }
 
   del() {
-    this.current = Math.floor(this.current / 10); // remove last number
+    if (this.current === '0') {
+      return Display.errorMessage();
+    } else if (this.current.length === 1) {
+      this.current = '0';
+    } else {
+      this.current = this.current.slice(0, this.current.length - 1);
+    }
     Display.mainScreen(this.current);
   }
 
-  operate() {
-    const currentObj = this;
-    const table = {
-      '+': currentObj.sum,
-      '-': currentObj.sub,
-      '/': currentObj.divide,
-      '*': currentObj.multiply,
-    };
-    return table[this.operator]();
+  operate(operator) {
+    if (operator === '+') return this.sum();
+    if (operator === '-') return this.sub();
+    if (operator === '/') return this.divide();
+    return this.multiply();
   }
 
+  update() {}
+
   sum() {
+    this.previous += this.current;
+    Display.supportScreen(this.previous, '+');
+    this.current = '0';
     this.result = this.previous + this.current;
-    this.update();
   }
 
   sub() {
     this.result = this.previous - this.current;
-    this.update();
   }
 
   divide() {
     this.result = this.previous / this.current;
-    this.update();
   }
 
   multiply() {
     this.result = this.previous * this.current;
-    this.update();
   }
 
   equal() {
@@ -82,21 +105,31 @@ class Calculator {
   }
 
   clear() {
-    this.previous = 0;
-    this.current = 0;
-    this.operator = '+';
+    this.previous = '0';
+    this.current = '0';
+    this.result = '0';
     Display.supportScreen('', '');
     Display.mainScreen(this.current);
   }
 
   percent() {
-    this.equal();
+    // this.equal();
+  }
+
+  negation() {
+    if (this.current === '0') return Display.errorMessage();
+    if (this.current[0] === '-') {
+      this.current = this.current.slice(1);
+      return Display.mainScreen(this.current);
+    }
+    this.current = '-' + this.current;
+    return Display.mainScreen(this.current);
   }
 }
 
 window.addEventListener('DOMContentLoaded', (e) => {
   const app = new Calculator();
-  app.clear(); // reset when start
+  app.clear();
 
   // define all buttons
   const ac = document.getElementById('btn-ac');
@@ -108,7 +141,33 @@ window.addEventListener('DOMContentLoaded', (e) => {
   const operations = document.querySelectorAll('[data-operator]');
 
   // all buttons' even listeners
-  ac.addEventListener('click', app.clear);
+  ac.addEventListener('click', (e) => {
+    app.clear();
+  });
 
-  ce.addEventListener('click', app.del);
+  ce.addEventListener('click', (e) => {
+    app.del();
+  });
+
+  percent.addEventListener('click', (e) => app.percent());
+
+  numbers.forEach((number) =>
+    number.addEventListener('click', (e) => {
+      app.add(e.target.value); // this must be string
+    })
+  );
+
+  operations.forEach((operation) =>
+    operation.addEventListener('click', (e) => {
+      app.operate(e.target.value);
+    })
+  );
+
+  equal.addEventListener('click', (e) => {
+    app.equal();
+  });
+
+  negation.addEventListener('click', (e) => {
+    app.negation();
+  });
 });
